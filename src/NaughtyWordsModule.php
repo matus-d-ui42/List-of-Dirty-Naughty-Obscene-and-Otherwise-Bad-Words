115bf3ce445b7a5d276120daf47d7d9a3866c92f
Matúš Demko <matus.demko@ui42.sk>
2021-10-20 11:45:42 +0200

    fix module class

--- src/NaughtyWordsModule.php
+++ src/NaughtyWordsModule.php
@@ -1,17 +1,18 @@
 <?php
 
 namespace Buxus\NaughtyWords;
- 
+
+use Buxus\Module\BuxusModule;
 
 class NaughtyWordsModule extends BuxusModule
 {
     public function register()
     {
-        
+
     }
 
     public function boot()
     {
- 
+
     }
-?>
+}
